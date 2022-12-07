3e89e07c548d8bd4a779ee29e85fba56d6780fe2
Rouslan Lakhno <116111453+rlakhno@users.noreply.github.com>
2022-12-07 12:03:41 -0500

    Update HttpStatusImageDownloader.java
    Fixed Exception handling.

--- src/main/java/homeWork/http/HttpStatusImageDownloader.java
+++ src/main/java/homeWork/http/HttpStatusImageDownloader.java
@@ -15,15 +15,21 @@ public class HttpStatusImageDownloader {
         String extension = ".jpg";
         HttpStatusChecker httpStatusChecker = new HttpStatusChecker();
         String catUrl = httpStatusChecker.getStatusImage(code);
-        try{
-            URL url = new URL(catUrl);
-            image = ImageIO.read(url);
-            ImageIO.write(image, "jpg", new File("./catPics" + code
-                    + extension));
-            System.out.println("PICTURE WITH URL: " + url + " WAS DOWNLOADED <-- ");
-        } catch (IOException e){
-            e.printStackTrace();
-            System.out.println(" -- Something went wrong !!! --- ");
+        
+        if (catUrl.contains("invalidStatus")) {
+            System.out.println("!!! --> WARNING: THE STATUS CODE: " + code + " IS OUT OF RANGE <-- ");
+        } else {
+            try {
+                URL url = new URL(catUrl);
+                image = ImageIO.read(url);
+                ImageIO.write(image, "jpg", new File("./catPics" + code
+                        + extension));
+                System.out.println("PICTURE WITH URL: " + url + " WAS DOWNLOADED <-- ");
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println(" -- Something went wrong !!! --- ");
+
+            }
         }
 
 
