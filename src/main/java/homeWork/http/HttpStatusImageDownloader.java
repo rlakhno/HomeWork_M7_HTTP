a840191c24a1b6f58d5929a40f9168529a626264
Rouslan Lakhno <116111453+rlakhno@users.noreply.github.com>
2022-12-10 17:44:46 -0500

    Update HttpStatusImageDownloader.java

--- src/main/java/homeWork/http/HttpStatusImageDownloader.java
+++ src/main/java/homeWork/http/HttpStatusImageDownloader.java
@@ -8,31 +8,24 @@ import java.net.URL;
 
 public class HttpStatusImageDownloader {
 
-    void downloadStatusImage(int code) throws Exception {
-
+       void downloadStatusImage(int code) throws Exception {
 
         BufferedImage image = null;
         String extension = ".jpg";
         HttpStatusChecker httpStatusChecker = new HttpStatusChecker();
         String catUrl = httpStatusChecker.getStatusImage(code);
-        
-        if (catUrl.contains("invalidStatus")) {
-            System.out.println("!!! --> WARNING: THE STATUS CODE: " + code + " IS OUT OF RANGE <-- ");
-        } else {
-            try {
-                URL url = new URL(catUrl);
-                image = ImageIO.read(url);
-                ImageIO.write(image, "jpg", new File("./catPics" + code
-                        + extension));
-                System.out.println("PICTURE WITH URL: " + url + " WAS DOWNLOADED <-- ");
-            } catch (IOException e) {
-                e.printStackTrace();
-                System.out.println(" -- Something went wrong !!! --- ");
 
-            }
+        try {
+            URL url = new URL(catUrl);
+            image = ImageIO.read(url);
+            ImageIO.write(image, "jpg", new File("./catPics" + code
+                    + extension));
+            System.out.println("PICTURE WITH URL: " + url + " WAS DOWNLOADED <-- ");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println(" -- Something went wrong !!! --- ");
+
         }
-
-
     }
 
 }
