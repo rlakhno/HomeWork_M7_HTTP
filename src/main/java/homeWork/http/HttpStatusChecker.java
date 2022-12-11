f5c5f4af53c58eda90c772ccff8962697232dc1c
Rouslan Lakhno <116111453+rlakhno@users.noreply.github.com>
2022-12-10 19:02:28 -0500

    Update HttpStatusChecker.java

--- src/main/java/homeWork/http/HttpStatusChecker.java
+++ src/main/java/homeWork/http/HttpStatusChecker.java
@@ -4,26 +4,32 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class HttpStatusChecker {
+  // new option for exception
 
-    String getStatusImage(int code) throws MalformedURLException {
+
+    String getStatusImage(int code) {
 
         String catCodeUrl = "https://http.cat/" + code + ".jpg";
-        URL url = new URL(catCodeUrl);
         try {
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        URL url = new URL(catCodeUrl);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
-            connection.setRequestMethod("GET");
-            connection.setConnectTimeout(5000);
-            connection.setReadTimeout(5000);
-            int status = connection.getResponseCode();
+        connection.setRequestMethod("GET");
+        connection.setConnectTimeout(5000);
+        connection.setReadTimeout(5000);
+        int status = connection.getResponseCode();
 
 
-            if (status == 404) {
-                throw new MalformedURLException("page not found");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+        if (status == 404) {
+            throw new MalformedURLException("page not found");
         }
+        } catch (MalformedURLException | ProtocolException e) {
+            e.printStackTrace();
+            System.out.println("PAGE NOT FOUND");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
 
         return catCodeUrl;
     }
